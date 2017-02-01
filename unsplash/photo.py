2ab4a5b2e500724643769ae63825edb47ad2dc98
Yakup Adaklı <yakup.adakli@gmail.com>
2017-02-01 10:21:32 +0300

    search and get functions added to photo

--- unsplash/photo.py
+++ unsplash/photo.py
@@ -6,32 +6,45 @@ class Photo(Client):
     def __init__(self, **kwargs):
         super(Photo, self).__init__(**kwargs)
         self.ordering_values = ["latest", "oldest", "popular"]
+        self.orientation_values = ["landscape", "portrait", "squarish"]
+
+    def _all(self, url, page=1, per_page=10, order_by="latest"):
+        if order_by not in self.ordering_values:
+            raise Exception()
+        params = {
+            "page": page,
+            "per_page": per_page,
+            "order_by": order_by
+        }
+        response = self._get(url, params=params)
+        return response.json()
 
     def all(self, page=1, per_page=10, order_by="latest"):
-        if order_by not in self.ordering_values:
-            raise Exception()
-        url = "/photos"
-        params = {
-            "page": page,
-            "per_page": per_page,
-            "order_by": order_by
-        }
-        response = self._get(url, params=params)
-        return response.json()
+        return self._all("/photos", page=page, per_page=per_page, order_by=order_by)
 
     def curated(self, page=1, per_page=10, order_by="latest"):
-        if order_by not in self.ordering_values:
-            raise Exception()
-        url = "/photos/curated"
+        return self._all("/photos/curated", page=page, per_page=per_page, order_by=order_by)
+
+    def get(self, photo_id, width=None, height=None, rect=None):
+        url = "/photos/%s" % photo_id
         params = {
-            "page": page,
-            "per_page": per_page,
-            "order_by": order_by
+            "width": width,
+            "height": height,
+            "rect": rect
         }
         response = self._get(url, params=params)
         return response.json()
 
-    def get(self, photo_id):
-        url = "/photos/%s" % photo_id
-        response = self._get(url)
+    def search(self, query, category=None, orientation=None, page=1, per_page=10):
+        if orientation and orientation not in self.orientation_values:
+            raise Exception()
+        params = {
+            "query": query,
+            "category": category,
+            "orientation": orientation,
+            "page": page,
+            "per_page": per_page
+        }
+        url = "/photos/search"
+        response = self._get(url, params=params)
         return response.json()
