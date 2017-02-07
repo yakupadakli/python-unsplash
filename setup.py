29d8298da16ebef67526fabecc50dbd746b97cac
Yakup Adaklı <yakup.adakli@gmail.com>
2017-02-07 13:59:18 +0300

    setup added.

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import uuid
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_requirements = parse_requirements('requirements.txt', session=uuid.uuid1())
+requirements = [str(req.req) for req in install_requirements]
+
+setup(
+    name="python-unsplash",
+    version="1.0.0",
+    description="A Python client for the Unsplash API.",
+    license="MIT",
+    author="Yakup Adaklı",
+    author_email="yakup.adakli@gmail.com",
+    url="http://github.com/yakupadakli/python-unsplash.git",
+    packages=find_packages(exclude=['tests']),
+    install_requires=requirements,
+    keywords="unsplash library",
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+    zip_safe=True
+)
