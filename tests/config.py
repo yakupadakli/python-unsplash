3e33013e82e6b4c41ed7a8d5b90b85d5ce57a025
Yakup Adaklı <yakup.adakli@gmail.com>
2017-02-08 17:33:58 +0300

    tests started, config added and test_photo finished.

---
+++ tests/config.py
@@ -0,0 +1,60 @@
+import os
+import unittest
+
+from unsplash.auth import Auth
+from unsplash.api import Api
+
+client_id = os.environ.get('CLIENT_ID', '')
+client_secret = os.environ.get('CLIENT_SECRET', '')
+redirect_uri = os.environ.get('REDIRECT_URI', '')
+
+# Sample Authorization code
+code = ""
+
+# Sample token
+token = {
+    u'access_token': u'f7cb80ed7ac6f32720b30cf664cb09393479dbdcd8a277e92ae1ed3529ab60e4',
+    u'created_at': 1486560652,
+    u'refresh_token': u'8f6484c0f34da29f89d1ae5e5753534636d619338c3b980a63156282504decdf',
+    u'scope': [u'public',
+               u'read_user',
+               u'write_user',
+               u'read_photos',
+               u'write_photos',
+               u'write_likes',
+               u'write_followers',
+               u'read_collections',
+               u'write_collections'],
+    u'token_type': u'bearer'
+}
+
+scope = [
+    "public",
+    "read_user",
+    "write_user",
+    "read_photos",
+    "write_photos",
+    "write_likes",
+    "write_followers",
+    "read_collections",
+    "write_collections"
+]
+
+
+class UnsplashTestCase(unittest.TestCase):
+    default_user_name = u'yakupa'
+    is_authenticated = False
+    scope = scope
+    token = token
+    code = code
+
+    def setUp(self):
+        if self.token:
+            self.auth = Auth(client_id, client_secret, redirect_uri, scope=scope, token=token)
+            self.is_authenticated = True
+        elif self.code:
+            self.auth = Auth(client_id, client_secret, redirect_uri, scope=scope, code=code)
+            self.is_authenticated = True
+        else:
+            self.auth = Auth(client_id, client_secret, redirect_uri, scope=scope)
+        self.api = Api(self.auth)
---
+++ tests/test_photo.py
@@ -0,0 +1,54 @@
+import unittest
+
+from .config import UnsplashTestCase
+from unsplash.models import Photo, Stat
+
+
+class PhotoTest(UnsplashTestCase):
+    setup_done = False
+    default_photo_id = u'KSap1iDftvQ'
+    default_search_query = u'nature'
+
+    def test_all(self):
+        photos = self.api.photo.all(per_page=2)
+        self.assertIsInstance(photos, list)
+        self.assertIsInstance(photos[0], Photo)
+        self.assertEqual(len(photos), 2)
+
+    def test_curated(self):
+        curated_photos = self.api.photo.curated(per_page=2)
+        self.assertIsInstance(curated_photos, list)
+        self.assertIsInstance(curated_photos[0], Photo)
+        self.assertEqual(len(curated_photos), 2)
+
+    def test_get(self):
+        photo = self.api.photo.get(self.default_photo_id)
+        self.assertIsNotNone(photo)
+
+    def test_search_photo(self):
+        photos = self.api.photo.search(self.default_search_query, per_page=2)
+        self.assertEqual(len(photos), 2)
+        self.assertIsInstance(photos, list)
+        self.assertIsInstance(photos[0], Photo)
+
+    def test_random(self):
+        photos = self.api.photo.random(count=2)
+        self.assertEqual(len(photos), 2)
+        self.assertIsInstance(photos, list)
+        self.assertIsInstance(photos[0], Photo)
+
+    def test_stats(self):
+        stat = self.api.photo.stats(self.default_photo_id)
+        self.assertIsInstance(stat, Stat)
+
+    def test_like(self):
+        photo = self.api.photo.like(self.default_photo_id)
+        self.assertIsInstance(photo, Photo)
+
+    def test_unlike(self):
+        photo = self.api.photo.like(self.default_photo_id)
+        self.assertIsInstance(photo, Photo)
+
+
+if __name__ == '__main__':
+    unittest.main()
